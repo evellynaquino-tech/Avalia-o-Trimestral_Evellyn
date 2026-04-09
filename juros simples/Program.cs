ec07276fa12c9d80f120ab7cb02f82e5a3f257ab
evellynaquino-tech <evellyn.aquino@edu.senai.br>
2026-04-09 14:48:31 -0300

    Add files via upload

---
+++ juros simples/Program.cs
@@ -0,0 +1,17 @@
+﻿//Juros Simples - Evellyn Silva de Aquino
+
+Console.WriteLine("\nDigite o valor do Capital: ");
+int.TryParse(Console.ReadLine(), out int capital);
+Console.WriteLine($"O valor do seu capital é {capital}");
+
+Console.WriteLine("\nDigite o valor da taxa:");
+float.TryParse(Console.ReadLine(), out float taxa);
+Console.WriteLine($"O valor da sua taxa é {taxa}%");
+
+Console.WriteLine("\nDigite o valor do tempo:");
+float.TryParse(Console.ReadLine(), out float tempo);
+Console.WriteLine($"O valor do seu tempo é {tempo}");
+
+float resultado = capital * taxa * tempo;
+
+Console.WriteLine($"O valor do seu juros simples é: R${resultado}");
---
+++ numeros_primos/Program.cs
@@ -0,0 +1,24 @@
+﻿//Primos - Evellyn Silva de Aquino
+int cont = 0, resultado;
+Console.WriteLine("Calculadora de Primos: \nSelecione um número e veja se ele é primo ou não!");
+int.TryParse(Console.ReadLine(), out int numero);
+
+for (int divisor = 1; divisor <= numero; divisor++)
+{
+
+    if (numero % divisor == 0)
+    {
+
+        cont++;
+    }
+}
+
+if (cont == 2)
+{
+    Console.WriteLine($"\nO numero {numero} é primo!");
+
+}
+else
+{
+    Console.WriteLine($"\nO número {numero} não é primo!");
+}

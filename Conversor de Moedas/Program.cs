fc28b81dd03a0b5448c93b1ecc098b2243501084
evellynaquino-tech <evellyn.aquino@edu.senai.br>
2026-04-09 14:47:30 -0300

    Add files via upload

---
+++ Conversor de Moedas/Program.cs
@@ -0,0 +1,32 @@
+﻿//Conversor de Moedas - Evellyn Silva de Aquino
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+float Moeda, ConversorReal, Resultado;
+int Escolha;
+
+Console.WriteLine("Conversor de Moedas 🪙");
+Console.WriteLine("Digite o valor em Reais que deseja converter:    ");
+float.TryParse(Console.ReadLine()!, out Moeda);
+
+Console.WriteLine("\nDigite 1 para converter para Dólar \nDigite 2 para converter para Libras");
+int.TryParse(Console.ReadLine()!, out Escolha);
+
+switch (Escolha)
+{
+    case 1:
+        Console.WriteLine("Digite o valor atual do Dólar:    ");
+        float.TryParse(Console.ReadLine()!, out ConversorReal);
+        Resultado = Moeda * ConversorReal;
+        Console.WriteLine($"R${Moeda} x U${ConversorReal} = R${Resultado}");
+        break;
+    case 2:
+        Console.WriteLine("Digite o valor atual da Libra:    ");
+        float.TryParse(Console.ReadLine()!, out ConversorReal);
+        Resultado = Moeda * ConversorReal;
+        Console.WriteLine($"R${Moeda} x U${ConversorReal} = R${Resultado}");
+        break;
+    default:
+        Console.WriteLine("Digite um valor válido");
+        break;
+}
---
+++ IMC/Program.cs
@@ -0,0 +1,21 @@
+﻿//IMC - Evellyn Silva de Aquino
+
+Console.WriteLine("\nDigite a sua altura em m²");
+double.TryParse(Console.ReadLine()!.Replace(".", ","), out double altura);
+
+Console.WriteLine("Calculadora de IMC: \nDigite o seu peso:");
+double.TryParse(Console.ReadLine()!.Replace(".", ","), out double peso);
+
+double IMC = peso / Math.Pow(altura, 2.0);
+
+if (IMC <= 18.5)
+{ Console.WriteLine($"Você está abaixo do peso."); }
+
+else if (IMC >= 18.5)
+{ Console.WriteLine($"Você está em um peso ideal."); }
+
+else if (IMC >= 25)
+{ Console.WriteLine($"Você está em um peso com obesidade."); }
+
+else if (IMC >= 30)
+{ Console.WriteLine($"Você está em um peso com obesidade severa."); }
